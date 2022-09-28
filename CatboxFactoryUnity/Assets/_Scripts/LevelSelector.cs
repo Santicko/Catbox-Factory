11c4478f6b2937294e642307f18d43a17cbcd55b
Eirik <01eirikso@gmail.com>
2022-09-28 12:45:11 +0200

    Level Selection

--- CatboxFactoryUnity/Assets/_Scripts/LevelSelector.cs
+++ CatboxFactoryUnity/Assets/_Scripts/LevelSelector.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class LevelSelector : MonoBehaviour
 {
-    public int level;
+    public int levelID;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     public void OpenScene()
     {
-        SceneManager.LoadScene("Level " + level.ToString());
+        SceneManager.LoadScene("Level" + levelID);
     }
 }
