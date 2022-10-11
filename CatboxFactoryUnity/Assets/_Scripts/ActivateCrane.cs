c3962faa2bb0b5489e3431da78ee9ed5dde0b9a4
Santicko <Sanderervik@gmail.com>
2022-10-11 15:31:52 +0200

    Update

--- CatboxFactoryUnity/Assets/_Scripts/ActivateCrane.cs
+++ CatboxFactoryUnity/Assets/_Scripts/ActivateCrane.cs
@@ -18,6 +18,7 @@ public class ActivateCrane : MonoBehaviour
     public bool clickedCrane;
 
     public float timePassed = 0f;
+    public int speed = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -56,7 +57,7 @@ public class ActivateCrane : MonoBehaviour
         if (risingMovement)
         {
             Vector3 hitbox = upperHitbox.transform.position;
-            float moveRate = 1.5f * Time.deltaTime;
+            float moveRate = 2f * Time.deltaTime * speed;
             player.transform.position = Vector3.MoveTowards(player.transform.position, hitbox, moveRate);
 
             if (player.transform.position.y > hitbox.y - 0.1 && player.transform.position.y < hitbox.y + 0.1)
@@ -70,10 +71,10 @@ public class ActivateCrane : MonoBehaviour
         if (rotatingMovement)
         {
             timePassed += Time.deltaTime;
-            float rotationRate = 90f * Time.deltaTime;
+            float rotationRate = 90f * speed * Time.deltaTime;
             rotating.transform.Rotate(Vector3.down, rotationRate);
 
-            if (timePassed >= 2f)
+            if (timePassed >= 2f / speed)
             {
                 timePassed = 0f;
                 craneHitbox.GetComponent<ActivateCrane>().rotatingDone = true;
