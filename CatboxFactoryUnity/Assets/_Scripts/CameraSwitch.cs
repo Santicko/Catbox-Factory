d3d367b02ad9177c5a40b802a6eff39ee528646e
Santicko <Sanderervik@gmail.com>
2022-10-21 14:49:08 +0200

    update

--- CatboxFactoryUnity/Assets/_Scripts/CameraSwitch.cs
+++ CatboxFactoryUnity/Assets/_Scripts/CameraSwitch.cs
@@ -9,17 +9,30 @@ public class CameraSwitch : MonoBehaviour
 
     public GameObject Cam2;
 
+    public GameObject Cam3;
+
     public void Camera1()
     {
         Cam1.SetActive(true);
         Cam2.SetActive(false);
+        Cam3.SetActive(false);
     }
 
     public void Camera2()
     {
-        {
+        
             Cam1.SetActive(false);
             Cam2.SetActive(true);
-        }
+            Cam3.SetActive(false);
+        
+    }
+
+    public void Camera3()
+    {
+        
+            Cam1.SetActive(false);
+            Cam2.SetActive(false);
+            Cam3.SetActive(true);
+        
     }
 }
