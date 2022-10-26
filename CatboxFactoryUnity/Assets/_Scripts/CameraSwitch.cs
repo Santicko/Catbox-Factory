0fbbb8858ce89c3134fddfe73d570ba2b8d68f6f
MikeNoLife <mikenolife@gmail.com>
2022-10-26 12:35:12 +0200

    level changes

--- CatboxFactoryUnity/Assets/_Scripts/CameraSwitch.cs
+++ CatboxFactoryUnity/Assets/_Scripts/CameraSwitch.cs
@@ -6,33 +6,37 @@ public class CameraSwitch : MonoBehaviour
 {
 
     public GameObject Cam1;
-
     public GameObject Cam2;
-
     public GameObject Cam3;
 
     public void Camera1()
     {
         Cam1.SetActive(true);
         Cam2.SetActive(false);
-        Cam3.SetActive(false);
+        if (Cam3 != null)
+        {
+            Cam3.SetActive(false);
+        }
     }
 
     public void Camera2()
     {
-        
-            Cam1.SetActive(false);
-            Cam2.SetActive(true);
+        Cam1.SetActive(false);
+        Cam2.SetActive(true);
+        if (Cam3 != null)
+        {
             Cam3.SetActive(false);
-        
+        }
+
     }
 
     public void Camera3()
     {
-        
-            Cam1.SetActive(false);
-            Cam2.SetActive(false);
+        Cam1.SetActive(false);
+        Cam2.SetActive(false);
+        if (Cam3 != null)
+        {
             Cam3.SetActive(true);
-        
+        }
     }
 }
