d2804b450886b649120c06f4e92b3817df465f95
MikeNoLife <mikenolife@gmail.com>
2022-10-26 11:56:39 +0200

    Level select script and exit sign

--- CatboxFactoryUnity/Assets/_Scripts/LevelSelector.cs
+++ CatboxFactoryUnity/Assets/_Scripts/LevelSelector.cs
@@ -1,22 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 
 public class LevelSelector : MonoBehaviour
 {
-    public int levelID;
+    private string levelName;
+    private int levelNumber;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        Scene scene;
+        scene = SceneManager.GetActiveScene();
+        levelName = scene.name;
+        string[] x = levelName.Split('_');
 
+        levelNumber = int.Parse(x[1]);
     }
 
     // Update is called once per frame
     public void OpenScene()
     {
-        SceneManager.LoadScene("Level0" + levelID);
+        SceneManager.LoadScene("Level" + (levelNumber+1));
+    }
+
+    public void LevelClick()
+    {
+        string levelButtonName = EventSystem.current.currentSelectedGameObject.name;
+        SceneManager.LoadScene("Level_" + levelButtonName);
     }
 }
