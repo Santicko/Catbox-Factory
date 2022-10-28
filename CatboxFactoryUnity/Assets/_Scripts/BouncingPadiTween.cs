cef5181a7a6bfd4e00eebb0ece380d151b6088b1
Santicko <Sanderervik@gmail.com>
2022-10-28 16:08:55 +0200

    Update

--- CatboxFactoryUnity/Assets/_Scripts/BouncingPadiTween.cs
+++ CatboxFactoryUnity/Assets/_Scripts/BouncingPadiTween.cs
@@ -14,6 +14,6 @@ public class BouncingPadiTween : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        iTween.MoveTo(other.tag == "Player", iTween.Hash("x", 9, "time", 4, "delay", 1, "onupdate", "myUpdateFunction", "looptype", iTween.LoopType.none));
+        iTween.MoveTo(gameObject, iTween.Hash("movetopath", "x", 9, "time", 4, "delay", 1, "onupdate", "myUpdateFunction", "looptype", iTween.LoopType.none));
     }
 }
--- CatboxFactoryUnity/Assets/_Scripts/PressAnimation.cs
+++ CatboxFactoryUnity/Assets/_Scripts/PressAnimation.cs
@@ -7,7 +7,7 @@ public class PressAnimation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        iTween.MoveTo(gameObject, iTween.Hash("y", 1, "time", 2f, "delay", 0.2f, "onupdate", "myUpdateFunction", "looptype", iTween.LoopType.pingPong));
+        iTween.MoveTo(gameObject, iTween.Hash("y", 1, "time", 0.5f, "delay", 0.5f, "onupdate", "myUpdateFunction", "looptype", iTween.LoopType.pingPong));
     }
 
     // Update is called once per frame
