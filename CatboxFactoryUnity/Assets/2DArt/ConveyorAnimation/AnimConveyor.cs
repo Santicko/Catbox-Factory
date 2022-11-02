7da83a890db88ab95d4a144d3fd7afec9eda424a
MikeNoLife <mikenolife@gmail.com>
2022-11-02 14:53:27 +0100

    preparing new build

--- CatboxFactoryUnity/Assets/2DArt/ConveyorAnimation/AnimConveyor.cs
+++ CatboxFactoryUnity/Assets/2DArt/ConveyorAnimation/AnimConveyor.cs
@@ -21,7 +21,7 @@ public class AnimConveyor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animSpeed = transform.root.GetComponentInChildren<ConveyorController>().gameSpeed * 4;
+        animSpeed = transform.root.GetComponentInChildren<ConveyorController>().gameSpeed * 9f;
         if (timer > 0) { timer -= Time.deltaTime * animSpeed; }
         else
         {
