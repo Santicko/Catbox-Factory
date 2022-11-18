14a91539419d3eab757ccd15afa478b9516ed51e
Santicko <Sanderervik@gmail.com>
2022-11-18 09:42:38 +0100

    update

--- CatboxFactoryUnity/Assets/_Scripts/PressAnimation.cs
+++ CatboxFactoryUnity/Assets/_Scripts/PressAnimation.cs
@@ -9,6 +9,6 @@ public class PressAnimation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        iTween.MoveTo(gameObject, iTween.Hash("y", 1, "time", animationTime, "delay", animationDelay, "onupdate", "myUpdateFunction", "looptype", iTween.LoopType.pingPong));
+        iTween.MoveTo(gameObject, iTween.Hash("y", 1.1, "time", animationTime, "delay", animationDelay, "onupdate", "myUpdateFunction", "looptype", iTween.LoopType.pingPong));
     }
 }
